d40daffa9c347d29480f56368aebdf67a808bfc3
Eysteix <vision060607@gmail.com>
2024-04-06 12:47:41 +0000

    "New Website For Sir"

---
+++ Js/main.js
@@ -0,0 +1,34 @@
+// async function API(){
+//     const data  = await fetch('/js/api.json',{cache:"no-store"});
+//     const res =  await data.json();
+
+//     JSON.stringify(res);
+
+//    const body = res.map((item) => {
+//         return(
+//             `
+//             <div class="box">
+//             <h1>${item.name}</h1>
+//             <p>${item.genre}</p>
+//             </div>
+//             `
+//         )
+//     }).join("");
+
+
+//      document.querySelector("div.boxes").innerHTML = body;
+
+// }
+
+// API();
+
+
+
+
+function hide(){
+    document.querySelector(".mobile-menu").classList.add("hidden");
+}
+function show(){
+    document.querySelector(".mobile-menu").classList.toggle("hidden");
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{js,html}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      pri:"Montserrat",
+      sec:"Poppins",
+      ter:"Mooli"
+    }
+  },
+  plugins: [],
+}
+
